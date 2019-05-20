6717f65069a0dd551d5ddc1bfafb0200023c5e45
enisshallciii <44702416+enisshallciii@users.noreply.github.com>
2019-05-20 16:23:34 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,14 +1,25 @@
-<?php
-    <div id="header">
-    <div style="width: 50%;float: left; margin-top: 10px; "><a class="logo" style="color:rgb(227, 227, 227);text-decoration: none;" href="index.html";>Miami University</a></div>
-        <div style="width: 50%; float: right;">
-            <div><a class="hyperlink" href="Company.html" style="color: black;text-decoration: overline;">COMPANY</a></div>
-            <div><a class="hyperlink" href="services.html">SERVICES</a></div>
-            <div><a class="hyperlink" href="Contact.html">CONTACT</a></div>
-            <div><a class="hyperlink" href="Gallery.html">GALLERY</a></div>
 
+<!DOCTYPE html>
+<html manifest="manifest.appcache">
+<head>
+
+  <link rel="stylesheet" href="css/servicesstyle.css">
+  <link rel="stylesheet" href="css/servicesstyle1.css">
+    <title>Services</title>
+     
+</head>
+
+<body>
+
+<div id="header">
+        <div style="width: 100%;">
+            <div style="width: 50%;float: left; margin-top: 10px; "><a class="logo" style="color:rgb(227, 227, 227);text-decoration: none;" href="index.html";>Miami University</a></div>
+            <div style="width: 50%; float: right;">
+                <div><a class="hyperlink" href="Company.html" href="javascript:void(0)">COMPANY</a></div>
+                <div><a class="hyperlink" href="services.html" href="javascript:void(0)" style="color: black;text-decoration: overline;">SERVICES</a></div>
+                <div><a class="hyperlink" href="Contact.html" href="javascript:void(0)">CONTACT</a></div>
+                <div><a class="hyperlink" href="Gallery.html" href="javascript:void(0)">GALLERY</a></div>
+            </div>
         </div>
-    </div>
 </div>
-
-?>
+ 
