f66342f5c7ede02858ca1b1f3da5b438bc8d7255
Drenica <44572473+Drenica@users.noreply.github.com>
2019-05-22 23:06:55 +0200

    Headeri ne faqe me php

---
+++ INT/header.php
@@ -0,0 +1,63 @@
+<header>
+    
+        <head>
+            <title>LUXURY JEWELLERY</title>
+            <meta charset="UTF-8">
+            <meta name="viewport" , content="width=device-width, initial-scale=1.0">
+            <link href="styleheader.css" type="text/css" rel="stylesheet">
+            
+                 
+    </head> 
+        <body>
+        
+        <div class="top" style="font-size:12px;">
+            <ul id="left-navheader">
+                <li>Call Us Now:+383 227788</li>
+                <li>Email:<a href="mailto:luxury.jewelry@luxjewelry.com">luxury.jewelry@luxjewelry.com </a></li>
+                </ul>
+                
+                <ul id="right-navheader">
+                <li><a href="signup.php">Sign Up</a></li>
+                <li>| <a href="login.php">Login</a></li>
+                <li>| <a href="https://blog.feedspot.com/jewelry_rss_feeds/">RSS Feeds</a></li>
+                </ul>
+            
+            </div>
+        
+        
+            <div class="molecular">
+             
+                <div>
+                <table>
+                    <tr>
+                        <td width="400px" style="font-size:25px;"><font color="white" ><big><big>L</big></big>UXURY <big><big>J</big></big>EWELRY
+                        </font></td>
+                        <td> <br><br>
+                            <font size="2px"> 
+                                <a href="LJ.php">HOME</a>
+                                <a href="aboutus.php">ABOUT US</a>  
+                                <a href="Produktet-Rings.php">RINGS</a>
+                                <a href="Produktet-Earrings.php">EARRINGS</a> 
+                                <a href="Bracelet.php">BRACELETS</a>
+                                <a href="contactus.php">CONTACT US</a>
+                                
+                            </font>
+                            </td>
+                    </tr>
+                    
+                    
+                
+                </table>
+            </div>
+        
+            </div>
+        
+
+        
+        
+        </body>
+
+
+
+    </header>
+
