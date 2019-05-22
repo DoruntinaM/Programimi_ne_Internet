7c6158107c25ed82e96abc81d0af68f325895bb4
endritbajrami <43909587+endritbajrami@users.noreply.github.com>
2019-05-23 00:01:00 +0200

    Create SAFIR.php

---
+++ INT/SAFIR.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html>
+        <?php include ('header.php'); ?>
+    <body>
+        <head>
+            <title>Sapphire</title>
+             <link rel="stylesheet" href="nenfaqe3.css">
+                
+                </head>
+        <br>
+        
+        <div style="text-align: center";><h2>THE PRECIOUS GEMSTONE SAPPHIRE</h2></div>
+        <br>
+        <style>
+        img {
+              border-radius: 50%;
+             }
+</style>
+        <img src="pictures/safiri.jpg" alt="Blue SAPPHIRE Stone" height="400" width="400" >
+        <img src="pictures/safiri1.jpg" alt="Viloet SAPPHIRE Stone" height="400" width="400" >
+        <img src="pictures/safiri2.jpg" alt="Yellow SAPPHIRE Stone" height="400" width="400" >
+        <br>
+     <div style="align-content: center; padding:35px;">
+     Sapphire is a precious gemstone. It is typically blue, but natural "fancy" sapphires 
+     also occur in yellow, purple, orange,purple,blue and green colors; "parti sapphires" show two or
+     more colors.   The only color that sapphire cannot be is red – as red colored corundum 
+     is called ruby,another corundum variety. Pink colored corundum may be either classified 
+     as ruby or sapphire depending on locale. Commonly, natural sapphires are cut and polished
+     into gemstones and worn in jewelry. They also may be created synthetically in laboratories 
+     for industrial or decorative purposes in large crystal boules.</div>
+
+        <table>
+ 
+        <tr>
+          <td>Chemical formula:</td>
+            <td><b>α-Al2O3</b></td>
+        </tr>
+        <tr>
+          <td>Color:</td>
+            <td><b>It varies from the taste</b></td>
+        </tr>
+        <tr>
+          <td>Hardness:</td>
+            <td><b>9</b></td>
+        </tr>
+        <tr>
+          <td>Crystal System:</td>
+            <td><b>Trigonal</b></td>
+        </tr>
+        <tr>
+          <td>Refractive index:</td>
+            <td><b>1.768–1.772</b></td>
+        </tr>
+        <tr>
+          <td>Sg:</td>
+            <td><b>4.0~4.1</b></td>
+        </tr>
+        <tr>
+          <td>Transparency: </td>
+            <td><b>Transparent to opaque</b></td>
+        </tr>
+        <tr>
+          <td>Double Refraction:</td>
+            <td><b>– 0.008</b></td>
+        </tr>
+        <tr>
+          <td>Luster:</td>
+            <td><b>Vitreous</b></td>
+        </tr>
+         <tr>
+          <td>Cleavage:</td>
+             <td><b>Lacks cleavage</b></td>
+        </tr>
+         <tr>
+          <td>Mineral Class:</td>
+             <td><b>Corundum</b></td>
+        </tr>
+      </table>
+        <br>
+        
+  <style>      
+div#myDivrotate {
+  -ms-transform: rotate(20deg); /* IE 9 */
+  -webkit-transform: rotate(20deg); /* Safari */
+  transform: rotate(20deg); /* Standard syntax */
+}
+</style>
+<div id="myDivrotate">
+A kiss on the hand may feel very, very good,<br>
+but a diamond and sapphire bracelet lasts forever.<br>
+</div>
+        <style>
+            div#divtranslate {
+  background-color:azure;
+  width: 1200;
+  border: 1px solid black;
+  -ms-transform: translate(20px,20px); 
+  -webkit-transform: translate(20px,20px); 
+  transform: translate(20px,20px);
+        </style>
+        <div id="divtranslate" style="text-align:center">
+ "One of his tears fell in my mouth, where it became a blue sapphire, source of strength, source of strength and eternal hope"-Anita Diament.
+</div><br>
+        <br><br>
+        
+  <style>
+div#divscale {
+  width: 1000;
+  background-color:aquamarine;
+  border: 1px solid black;
+  -ms-transform: scale(1,2); 
+  -webkit-transform: scale(1,2); 
+  transform: scale(1,2); /
+}
+</style>   
+        <div id="divscale" style="text-align: center">
+" Truth is like heat or light; its vibrations are endless, and are endlessly felt."
+</div>
+        <br>
+        <br>
+        <style>
+    div#myDivskew {
+  background-color: aliceblue;
+  border-width: thick;
+  width: 800;
+  border: 1px dotted black;
+  -ms-transform: skew(25deg); 
+  -webkit-transform: skew(25deg); 
+  transform: skew(25deg); 
+}
+        </style>
+        <div id="myDivskew">
+"I don't wear perfume and I rarely wear jewelry!"
+</div>
+        <br>
+        <style>
+        #myDivrotatex {
+ background-color: aliceblue;
+  border-width: thick;
+  width: 600;
+border: 1px dashed black;
+  -webkit-transform: rotateX(150deg); 
+  transform: rotateX(150deg); 
+}
+    </style>  
+        <div id="myDivrotatex" style="text-align: center">
+"My favorite thing in wardrobe is my jewelry."
+</div>
+        <br>
+           <style>
+        #myDivrotatey {
+ background-color: cadetblue;
+ color: black;
+  border-width: thick;
+  width: 900;
+border: 1px double black;
+  -webkit-transform: rotateY(170deg); 
+  transform: rotateY(170deg); 
+}
+    </style>  
+        <div id="myDivrotatey" style="text-align: center">
+"I love jewelry - gold and diamonds. I'm a woman."
+</div>
+        <br>
+        <style> 
+div#divkey {
+  width: 250px;
+  height: 75px;
+  background: red;
+  color: black;
+  position: relative;
+  -webkit-animation: mymove 6s infinite; 
+  animation: mymove 6s infinite;
+}
+@-webkit-keyframes mymove {
+  0%   {top: 0px; background: black; width: 150px;}
+  100% {top: 100px; background: white; width: 350px;}
+}
+@keyframes mymove {
+  0%   {top: 0px; background: black; width: 150px;}
+  100% {top: 100px; background: white; width: 350px;}
+}
+</style>
+        <div id="divkey" ><i><b>"Jewelry takes people’s minds off your wrinkles!"</b></i></div>
+        <br><br><br><br><br><br><br><br>
+        
+        <style>
+        div#divresize {
+  border-width:5px;  
+border-style:dotted solid;
+  padding: 20px; 
+  width: 720px;
+  resize: horizontal;
+  overflow: auto;
+  outline: 4px solid black;
+  outline-offset: 15px;
+}
+            
+</style>
+
+<div id=divresize>
+  <p>1."I have always felt a gift diamond shines so much better than one you buy for yourself."</p>
+  <p>2."The pearl is the queen of gems and the gem of queens"</p>
+    <p>3."There is in them a softer fire than the ruby, there is the brilliant purple of the amethyst, and the sea green of the emerald - all shining together in incredible union.  Some by their splendor rival the colors of the painters, others the flame of burning sulphur or of fire quickened by oil."</p>
+    <p>4."I never worry about diets.  The only carrots that interest me are the number of carats in a diamond."</p>
+</div>
+<br>
+    <br>
+    </body>
+
+    <?php include ('footer.php'); ?>
+    
+         </html>
