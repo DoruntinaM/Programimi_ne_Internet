b1a1b21c0f608f39f125fb49b6cf51fb75ae9228
endritbajrami <43909587+endritbajrami@users.noreply.github.com>
2019-05-22 23:59:55 +0200

    Create Bracelet.php

---
+++ INT/Bracelet.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html>
+  
+<?php include ('header.php'); ?>
+
+<body>
+  <head>
+
+<title>Aksesoret</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="body.html">
+
+<style>
+.mySlides {display:none;}
+.row {
+  display: flex;
+  margin-left: 20px;
+  margin-right: 20px;
+  margin-top: 40px;
+  margin-bottom: 40px;
+}
+.row2 {
+  display: flex;
+  margin-left: 20px;
+  margin-right: 20px;
+  margin-top: 40px;
+  margin-bottom: 40px;
+}
+.column {
+  flex: 25%;
+  padding: 0px;
+}
+.card {
+  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.2);
+  max-width: 300px;
+  text-align: center;
+  font-family: arial;
+  margin-bottom: 20px;
+  margin-top: 20px;
+  background-color: antiquewhite;
+  
+}
+.price {
+  color: grey;
+  font-size: 22px;
+}
+.card button:hover {
+  opacity: 0.7;
+}
+div.t {
+  text-align: center;
+}
+</style>
+</head>
+
+
+
+
+
+
+
+<div class="content display-container">
+  <img class="mySlides" src="pictures/1.jpg" style="width:100%">
+  <img class="mySlides" src="pictures/2.jpg" style="width:100%">
+  <img class="mySlides" src="pictures/3.jpg" style="width:100%">
+  <img class="mySlides" src="pictures/4.jpg" style="width:100%">
+  <img class="mySlides" src="pictures/5.jpg" style="width:100%">
+  <img class="mySlides" src="pictures/6.jpg" style="width:100%">
+
+</div>
+
+<script>
+        var myIndex = 0;
+        carousel();
+        
+        function carousel() {
+          var i;
+          var x = document.getElementsByClassName("mySlides");
+          for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+          }
+          myIndex++;
+          if (myIndex > x.length) {myIndex = 1}    
+          x[myIndex-1].style.display = "block";  
+          setTimeout(carousel, 3000); 
+        }
+        </script>
+       
+       
+
+
+
+<div class="row">
+ 
+        <div class="column">
+<div class="card">
+    
+        <img src="pictures/pp1.jpg" alt="OW bracelet" style="width:100%">
+        <h1>Oliwer Weber</h1>
+        <p class="price">$89</p>
+        <p>Oliver Weber Swarovski Crystal Torc </p>
+        
+    </div>
+        </div>
+
+        
+        <div class="column">
+      <div class="card" >
+            <img src="pictures/pp2.jpg" alt="OW bracelet" style="width:100%">
+            <h1>Oliwer Weber</h1>
+            <p class="price">$98</p>
+            <p>Bracelet Couple rhod. crystal</p>
+            
+     </div>
+        </div>
+
+        <div class="column">
+        <div class="card" >
+            <img src="pictures/pp3.jpg" alt="Swarovski bracelet" style="width:100%">
+            <h1>Swarovski</h1>
+            <p class="price">$230</p>
+            <p>Swarovski Women's Rhodium Plating </p>
+            
+     </div>
+
+        </div>
+
+        <div class="column">
+     <div class="card">
+            <img src="pictures/pp4.jpg" alt="Swarovski bracelets" style="width:100%">
+            <h1>Swarovski</h1>
+            <p class="price">$580</p>
+            <p>Stainless statement tennis bracelet</p>
+            
+     </div>
+     </div>
+
+
+    </div>
+    <br>
+    <br>
+    <br>
+    <div class="row2">
+
+            <div class="column">
+    <div class="card">
+            <img src="pictures/pp5.jpg" alt="Swarovski bracelet" style="width:100%">
+            <h1>Swarovski</h1>
+            <p class="price">$3058</p>
+            <p>18K White Gold Overlay Tennis Bracelet</p>
+            
+        </div>
+            </div>
+    
+            
+            <div class="column">
+          <div class="card" >
+                <img src="pictures/pp6.jpg" alt="Ruby bracelet" style="width:100%">
+                <h1>Swarovski</h1>
+                <p class="price">$2800</p>
+                <p>Untreated Ruby and Pearl Bracelet</p>
+                
+         </div>
+            </div>
+    
+            <div class="column">
+            <div class="card" >
+                <img src="pictures/pp7.jpg" alt="Ruby earrings" style="width:100%">
+                <h1>Ruby</h1>
+                <p class="price">$1680</p>
+                <p>Delicate Retro 18k Yellow Gold Ruby </p>
+                
+         </div>
+    
+            </div>
+    
+            <div class="column">
+         <div class="card" >
+        <img src="pictures/pp8.jpg" alt="Diamond bracelet" style="width:100%">
+            <h1>Diamond </h1>
+            <p class="price">$15000</p>
+            <p>9ct White Gold 2.25ct Diamond Bracelet</p>
+                
+         </div>
+         </div>
+         </div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<div class="t">
+         <p style="font-size:35px;"><font color="pink" ><big><big>GIVE THE GIFT OF STYLE...</big></big>
+         </font></p>
+         <p>PROGRAM BENEFITS
+
+                The LUXURY JEWELLERY unites crystal enthusiasts of all ages from all over the world and offers its members a whole range of special benefits.
+                Join this fabulous community as a new member, renew your current membership, or offer <abbr title="LUXURY JEWELLERY">LJ</abbr> membership as a gift.
+                
+                </p>
+         </div>
+
+
+        <svg width="400" height="180">
+            <rect x="50" y="20" rx="20" ry="20" width="25" height="25"
+            style="fill:red;stroke:black;stroke-width:5;opacity:0.5" />
+          Sorry, your browser does not support inline SVG.
+          </svg>
+
+          
+        
+         
+           
+
+</body>
+
+<?php include ('footer.php'); ?>
+    
+     </html>
+
+
+
+
