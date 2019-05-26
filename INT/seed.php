b6ce616cb4c8ed9332f2c21c5b854e1aa44009fe
endritbajrami <43909587+endritbajrami@users.noreply.github.com>
2019-05-26 19:06:43 +0200

    Delete seed.php

--- INT/seed.php
+++
@@ -1,6 +0,0 @@
-<?php
-include("LidhjaDB.php");
-$db = new mysqli($host, $username, $password, $dbname, $port);
-$query = file_get_contents(__DIR__ . '/schema.sql');
-$error = $db->multi_query($query);
-echo $db->error;
