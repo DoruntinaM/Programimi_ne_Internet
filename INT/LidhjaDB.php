476a0587a7dab491ad447f4a6c0af0916453fa61
DoruntinaM <doruntina.murtezaj@studentet.uni-pr.edu>
2019-05-29 01:29:07 +0200

    Update LidhjaDB.php

--- INT/LidhjaDB.php
+++ INT/LidhjaDB.php
@@ -5,7 +5,23 @@ function connectDb() {
   $password = "";
   $dbname = "PI";
   $port = 3306;
-  
-  $db = new mysqli($host, $username, $password, $dbname,$port);
-  return $db;
+
+
+  try
+  {
+      if ($db = new mysqli($host, $username, $password, $dbname,$port))
+      {
+        return $db;
+      }
+      else
+      {
+          throw new Exception('Unable to connect');
+      }
+  }
+  catch(Exception $e)
+  {
+      echo $e->getMessage();
+  }
+
+ 
 }
